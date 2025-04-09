7abbf354fae95935807a7b0d35c6dabc739b4375
Douglas <douglasmsrodrigues@hotmail.com>
2025-04-09 10:29:13 -0300

    terminando as atulizacoes

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
 function scripts() {
     return gulp.src('./src/scripts/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('./dist/js'))
+        .pipe(gulp.dest('./dist/js'));
 }
 
 function styles() {
@@ -13,7 +15,13 @@ function styles() {
         .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = gulp.parallel(styles, scripts);
+function images() {
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, images, scripts);
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
